3547d799a06f77dfb73bb50ca6c144d3eb89c004
Karl Levik <karl.levik@diamond.ac.uk>
2016-08-23 16:00:05 +0100

    Additional methods, extra param for retrieve_current_sessions.

--- ispyb_api/core.py
+++ ispyb_api/core.py
@@ -30,7 +30,18 @@ class Core:
   def __init__(self):
     pass
 
-  # TODO: Implement stored function
+  def first_item_in_cursor(self, cursor):
+    rs = cursor.fetchone()
+    if len(rs) == 0:
+        return None
+    elif isinstance(cursor, MySQLdb.cursors.DictCursor):
+        return rs.iteritems().next()
+    else:
+        try:
+            return int(rs[0])
+        except:
+            return rs[0]
+
   _sample_params =\
     ExtendedOrderedDict([('id',None), ('crystalid',None), ('containerid',None), ('name',None), ('code',None), 
                          ('location',None), ('holder_length',None), ('loop_length',None), ('loop_type',None), 
@@ -56,42 +67,44 @@ class Core:
   def insert_sample(self, cursor, values):
     '''Store new sample.'''
     cursor.execute('select ispyb.upsert_sample(%s)' % ','.join(['%s'] * len(values)), values)
-    rs = cursor.fetchone()
-    if len(rs) > 0:
-        return int(rs[0])
-    return None
+    return self.first_item_in_cursor( cursor )
 
   def update_sample(self, cursor, values):
     '''Update existing sample.'''
     cursor.execute('select ispyb.upsert_sample(%s)' % ','.join(['%s'] * len(values)), values)
-    rs = cursor.fetchone()
-    if len(rs) > 0:
-        return int(rs[0])
-    return None
+    return self.first_item_in_cursor( cursor )
 
   def retrieve_visit_id(self, cursor, visit):
     '''Get the database ID for a visit on the form mx1234-5.'''
     cursor.execute('select ispyb.retrieve_visit_id(%s)', [visit])
-    rs = cursor.fetchone()
-    if len(rs) > 0:
-        return int(rs[0])
-    return None
+    return self.first_item_in_cursor( cursor )
 
   def retrieve_datacollection_id(self, cursor, img_filename, img_fileloc):
     '''Get the database ID for the data collection corresponding to the given diffraction image file.'''
     cursor.execute('select ispyb.retrieve_datacollection_id(%s,%s)', [img_filename, img_fileloc])
-    rs = cursor.fetchone()
-    if len(rs) > 0:
-        return int(rs[0])
-    return None
+    return self.first_item_in_cursor( cursor )
 
-  def retrieve_current_sessions(self, cursor, beamline):
+  def retrieve_current_sessions(self, cursor, beamline, tolerance_mins=0):
     '''Get a result-set with the currently active sessions on the given beamline.'''
-    cursor.callproc(procname='ispyb.retrieve_current_sessions', args=(beamline,))
+    cursor.callproc(procname='ispyb.retrieve_current_sessions', args=(beamline,tolerance_mins))
     rs = cursor.fetchall()
     cursor.nextset()
     return rs
 
+  def retrieve_current_sessions_for_person(self, cursor, beamline, fed_id, tolerance_mins=0):
+    '''Get a result-set with the currently active sessions on the given beamline.'''
+    cursor.callproc(procname='ispyb.retrieve_current_sessions_for_person', args=(beamline, fed_id, tolerance_mins))
+    rs = cursor.fetchall()
+    cursor.nextset()
+    return rs
+
+  def retrieve_most_recent_session(self, cursor, beamline, proposal_code):
+    '''Get a result-set with the most recent session on the given beamline for the given proposal code '''
+    cursor.callproc(procname='ispyb.retrieve_most_recent_session', args=(beamline, proposal_code))
+    rs = cursor.fetchall()
+    cursor.nextset()
+    return rs
+    
   def retrieve_current_cm_sessions(self, cursor, beamline):
     '''Get a result-set with the currently active commissioning (cm) sessions on the given beamline.'''
     cursor.callproc(procname='ispyb.retrieve_current_cm_sessions', args=(beamline,))
@@ -105,6 +118,11 @@ class Core:
     rs = cursor.fetchall()
     cursor.nextset()
     return rs
+
+  def retrieve_proposal_title(self, cursor, proposal_code, proposal_number):
+    '''Get the title of a given proposal'''
+    cursor.execute('select ispyb.retrieve_proposal_title(%s,%s)', [proposal_code, proposal_number])
+    return self.first_item_in_cursor( cursor )
 
 core = Core()
 
