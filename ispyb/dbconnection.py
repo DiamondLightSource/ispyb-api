592c85cf64e601155d523b5c4ce2f086dfa46dad
Karl Levik <karl.levik@diamond.ac.uk>
2017-02-17 12:34:36 +0000

    Removed misguided connect methods, added new connect method

--- ispyb/dbconnection.py
+++ ispyb/dbconnection.py
@@ -33,31 +33,18 @@ class DBConnection:
     self.cursor = None
     self.conn = None
     self.config = ConfigParser.RawConfigParser(allow_no_value=True)
-    #self.config.read( os.path.join( os.path.dirname( os.path.abspath(__file__) ), "credentials.cfg" ))
     self.config.readfp(codecs.open("/dls_sw/dasc/mariadb/credentials/ispyb_api.cfg", "r", "utf8"))
-    #read('/dls_sw/dasc/mariadb/credentials/ispyb_api.cfg')
     return
 
   def __del__(self):
     self.disconnect()
     return
 
-  def connect_to_prod(self, dict_cursor=False):
-    '''Create a connection to the production database'''
-    self.disconnect()
-    return self._connect(conf='prod', dict_cursor=dict_cursor)
-
-  def connect_to_dev(self, dict_cursor=False):
-    '''Create a connection to the development database'''
-    self.disconnect()
-    return self._connect(conf='dev', dict_cursor=dict_cursor)
-
-  def connect_to_stage(self, dict_cursor=False):
-    '''Create a connection to the test database'''
-    self.disconnect()
-    return self._connect(conf='stage', dict_cursor=dict_cursor)
-
   def _connect(self, conf='dev', dict_cursor=False):
+      return self.connect(conf, dict_cursor)
+      
+  def connect(self, conf='dev', dict_cursor=False):
+    self.disconnect()
     '''Create a connection to the database using the given parameters.'''
     self.conn = mysql.connector.connect(user=self.config.get(conf, 'user'), password=self.config.get(conf, 'pw'), \
                                 host=self.config.get(conf, 'host'), database=self.config.get(conf, 'db'), port=int(self.config.get(conf, 'port')))
