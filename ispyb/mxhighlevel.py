ea99c7b5b313f25334d95d8ce9324f3fa08cef26
Karl Levik <karl.levik@diamond.ac.uk>
2017-03-09 16:09:49 +0000

    High level MX API + test (a start ...)

---
+++ ispyb/mxhighlevel.py
@@ -0,0 +1,19 @@
+
+
+
+class MXHighLevel:
+  '''MXHighLevel provides high-level methods for storing MX results'''
+
+  def __init__(self):
+        pass
+
+  def store_i19_screen(self, dc_id, space_groups_and_unit_cells=None, dynamic_range=None, histogram=None):
+        '''Params:
+        dc_id - ISPyB dataCollectionId
+        space_groups_and_unit_cells - a list of tuples, each tuple consisting of a space group (string) and a list of 6 unit cell params  
+        dynamic_range - a float
+        histogram - a list of tuples
+        '''
+        pass
+
+mxhighlevel = MXHighLevel()
