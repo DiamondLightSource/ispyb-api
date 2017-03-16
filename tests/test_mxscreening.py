8b226023bbc0c522c544c817272258205c369528
Karl Levik <karl.levik@diamond.ac.uk>
2017-03-16 17:58:34 +0000

    Fix test bugs. Only works with dict_cursor=False.

--- tests/test_mxscreening.py
+++ tests/test_mxscreening.py
@@ -11,20 +11,28 @@ from nose import with_setup
 
 def get_dict_cursor():
     global cursor
-    cursor = dbconnection.connect_to_dev(dict_cursor=True) 
+    cursor = dbconnection.connect(conf='dev', dict_cursor=False) 
 
 def get_cursor():
     global cursor
-    cursor = dbconnection.connect_to_dev()
+    cursor = dbconnection.connect(conf='dev')
 
 def close_cursor():
     cursor.close()
     dbconnection.disconnect()
 
 def insert_screening(c):
+    visitid = core.retrieve_visit_id(cursor, 'cm5952-5')
+
     params = mxacquisition.get_data_collection_group_params()
-    params[''] = 'something'
-    id = mxacquisition.insert_data_collection_group(c, param.values())
+    params['parentid'] = visitid
+    # experimenttype must be one of the allowed values: None, 'SAD', 'SAD - Inverse Beam', 'OSC', 'Collect - Multiwedge', 'MAD', 'Helical', 'Multi-positional', 'Mesh',  'Burn', 'MAD - Inverse Beam', 'Screening'
+    params['experimenttype'] = 'OSC'
+    params['starttime'] = datetime.strptime('2017-03-15 13:00:00', '%Y-%m-%d %H:%M:%S')
+    params['endtime'] = datetime.strptime('2017-03-15 13:00:10', '%Y-%m-%d %H:%M:%S')
+    params['comments'] = 'This is a test of data collection group.'
+    #params[''] = 'something'
+    id = mxacquisition.insert_data_collection_group(c, params.values())
     
     params = mxscreening.get_screening_params()
     params['dcgid'] = id # dcgid from above ...
