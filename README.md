668771458c623abf0e17b1ff3c1a198585a4d4a8
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-08-23 12:51:46 +0100

    Start pypi package for ispyb

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://www.travis-ci.org/DiamondLightSource/python-ispyb.svg?branch=master)](https://www.travis-ci.org/DiamondLightSource/python-ispyb)
+
+# ISPyB pypi package
+This package provides a python interface to [ISPyB](http://www.esrf.eu/ispyb)
+via the official ISPyB webservices API or direct database access.
---
+++ setup.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, division
+from setuptools import setup, find_packages
+import sys
+
+# to release increment this number:
+package_version = '0.3'
+# and run the following on a bash prompt:
+'''
+export NUMBER="$(grep package_version setup.py | head -1 | cut -d"'" -f 2)";
+git add -u; git commit -m "v${NUMBER} release"; git tag -a v${NUMBER} -m v${NUMBER}; git push; git push origin v${NUMBER}
+python setup.py sdist upload
+'''
+
+if sys.version_info < (2,7):
+  sys.exit('Sorry, Python < 2.7 is not supported')
+
+setup(name='ispyb',
+      description='Python API for ISPyB',
+      url='https://github.com/DiamondLightSource/python-ispyb',
+      author='Markus Gerstel',
+      author_email='markus.gerstel@diamond.ac.uk',
+      download_url="https://github.com/DiamondLightSource/python-ispyb/releases",
+      version=package_version,
+      install_requires=['mysql-connector<2.2.3'],
+      packages=find_packages(),
+      license='BSD',
+      tests_require=['mock'],
+      classifiers = [
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+     )
