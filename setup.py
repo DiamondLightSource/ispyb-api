6a844942188386590ac89459e8a72118dd3e11b0
Kevin Savage <kevin.savage@diamond.ac.uk>
2017-08-23 14:35:15 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ispyb-api',
-    version='1.0.0',
+    version='1.0.1',
     description='ISPyB library',
     url='https://github.com/DiamondLightSource/ispyb-api',
     author='Karl Erik Levik',
