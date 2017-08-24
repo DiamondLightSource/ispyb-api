74e40bf344de59eed35a8f21762663a858fc718e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-08-24 13:44:38 +0100

    Ensure all drivers match API signatures

--- ispyb/test/driver/test_common.py
+++ ispyb/test/driver/test_common.py
@@ -1,6 +1,8 @@
 import inspect
 import ispyb.api.main
+import ispyb.driver.api
 import ispyb.driver.database
+import ispyb.driver.dummy
 import pytest
 
 @pytest.mark.incremental
@@ -8,10 +10,9 @@ class TestAPIImplementation(object):
   def test_create_main_api_object(self):
     ispyb.api.main.API()
 
-  def test_that_the_database_driver_implementation_matches_the_api_function_signatures(self):
+  def check_api(self, implementation):
     apiobj = ispyb.api.main.API()
     public_fn_names = filter(lambda name: not name.startswith('_'), dir(apiobj))
-    implementation = ispyb.driver.database.ISPyBDatabaseDriver
     implemented_functions = dir(implementation)
 
     for fn in public_fn_names:
@@ -19,3 +20,12 @@ class TestAPIImplementation(object):
              inspect.getargspec(getattr(apiobj, fn)), \
              "Implementation of function %s in database driver does not " \
              "match function signature in API definition" % fn
+
+  def test_that_the_api_driver_implementation_matches_the_api_function_signatures(self):
+    self.check_api(ispyb.driver.api.ISPyBAPIDriver)
+
+  def test_that_the_database_driver_implementation_matches_the_api_function_signatures(self):
+    self.check_api(ispyb.driver.database.ISPyBDatabaseDriver)
+
+  def test_that_the_dummy_driver_implementation_matches_the_api_function_signatures(self):
+    self.check_api(ispyb.driver.dummy.ISPyBDummyDriver)
