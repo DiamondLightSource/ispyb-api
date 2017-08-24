a8968e998431205567df12bd17bf196087bdfb96
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-08-24 09:09:03 +0100

    support three different backends

--- ispyb/common/driver.py
+++ ispyb/common/driver.py
@@ -1,7 +1,29 @@
-def get_driver(drivers, driver):
-  if driver in drivers:
-    mod_name, driver_name = drivers[driver]
-    _mod = __import__(mod_name, globals(), locals(), [driver_name])
-    return getattr(_mod, driver_name)
-  raise AttributeError('Driver %s does not exist' % driver)
+from __future__ import division, absolute_import
+from enum import Enum
 
+class Backend(Enum):
+  '''List of available backends for ispyb.'''
+  DUMMY    = ('Dummy driver',
+              'math', 'sin')
+  API      = ('Official API',
+              'math', 'sin')
+  DATABASE = ('Direct database access',
+              'math', 'sin')
+
+  def __init__(self, description, module, classname):
+    '''Make tuple elements reachable via attribute names.'''
+    self.description = description
+    self.module = module
+    self.classname = classname
+
+  def __repr__(self):
+    '''Slightly cleaner representation.'''
+    return self.description
+
+def get_driver(driver):
+  '''Factory function. Given a Backend type imports the relevant module and
+     returns the backend class which can then be instantiated.'''
+  if hasattr(driver, 'module') and hasattr(driver, 'classname'):
+    _mod = __import__(driver.module, globals(), locals(), [driver.classname])
+    return getattr(_mod, driver.classname)
+  raise AttributeError('Driver %s does not exist' % driver)
--- ispyb/test/common/test_driver.py
+++ ispyb/test/common/test_driver.py
@@ -2,7 +2,9 @@ import ispyb.common.driver
 import math
 import mock
 
-def test_getdriver_method_can_import_names_according_to_list():
-  mapping = { mock.sentinel.mathsin: ('math', 'sin') }
-  sinfunc = ispyb.common.driver.get_driver(mapping, mock.sentinel.mathsin)
+def test_getdriver_method_can_import_names_from_passed_object():
+  class pseudo_driver():
+    module = 'math'
+    classname = 'sin'
+  sinfunc = ispyb.common.driver.get_driver(pseudo_driver)
   assert math.sin == sinfunc
