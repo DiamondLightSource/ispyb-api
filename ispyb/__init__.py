65cca0c6769e694288d5626761f43d63df29a93b
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-08-25 13:17:08 +0100

    new release workflow

--- ispyb/__init__.py
+++ ispyb/__init__.py
@@ -2,7 +2,7 @@ from __future__ import division, absolute_import
 
 import ispyb.common.driver
 
-__version__ = "0.6"
+__version__ = "0.7"
 
 driver = ispyb.common.driver.get_driver
 Backend = ispyb.common.driver.Backend
