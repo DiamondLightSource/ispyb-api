c45cdfd834f50b3504fa44a14a1f6d10ed62ae4f
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-08-30 15:51:53 +0100

    v0.8 point-release

--- ispyb/__init__.py
+++ ispyb/__init__.py
@@ -2,7 +2,7 @@ from __future__ import division, absolute_import
 
 import ispyb.common.driver
 
-__version__ = "0.7"
+__version__ = "0.8"
 
 get_driver = ispyb.common.driver.get_driver
 Backend = ispyb.common.driver.Backend
