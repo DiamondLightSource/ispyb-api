332a8257f89c19665350620912180f118ff777bc
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-08-31 15:47:22 +0100

    new release version

--- ispyb/__init__.py
+++ ispyb/__init__.py
@@ -2,7 +2,7 @@ from __future__ import division, absolute_import
 
 import ispyb.common.driver
 
-__version__ = "0.10"
+__version__ = "0.11"
 
 get_driver = ispyb.common.driver.get_driver
 Backend = ispyb.common.driver.Backend
