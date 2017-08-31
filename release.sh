f20492ceb77ad98051f5b8acc27ffca86ddae889
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-08-31 08:55:40 +0100

    Must run clean before packaging, otherwise old files may be packaged.

--- release.sh
+++ release.sh
@@ -5,4 +5,5 @@ python -c "import sys; assert sys.hexversion >= 0x02070000, 'python too old'"
 export NUMBER=$(python -c "import ispyb; print ispyb.__version__")
 git tag -a v${NUMBER} -m v${NUMBER}
 git push origin v${NUMBER}
+python setup.py clean --all
 python setup.py sdist bdist_wheel upload
