37e8877f130cf879cec3a9ea8a1fa0f9cdffe441
Karl Levik <karl.levik@diamond.ac.uk>
2017-09-15 09:21:40 +0100

    module removed already, so removing test

--- tests/test_mxhighlevel.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-sys.path.append('..')
-from ispyb.mxhighlevel import mxhighlevel
-
-def test_store_i19_screen():
-    mxhighlevel.store_i19_screen(10010, space_groups_and_unit_cells=None, dynamic_range=None, histogram=None)
