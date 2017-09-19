ca147d362ba2bdb71cd3fc1b471348c7b7db6d40
Karl Levik <karl.levik@diamond.ac.uk>
2017-09-19 12:02:57 +0100

    Add keywords

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='ISPyB library',
     url='https://github.com/DiamondLightSource/ispyb-api',
     author='Karl Erik Levik',
+    keywords = ['ISPyB', 'database'], 
     packages=find_packages(),
     install_requires=['mysql-connector']
 )
