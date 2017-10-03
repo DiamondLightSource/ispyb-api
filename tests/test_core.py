b8050a3cdf7510038664f055ee393a69ad94a9a0
Karl Levik <karl.levik@diamond.ac.uk>
2017-10-03 22:55:16 +0100

    re-order tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,17 +4,22 @@ from datetime import datetime
 #from nose import with_setup
 from testtools import get_connection
 
+def test_upsert_sample():
+    conn = get_connection()
+    params = core.get_sample_params()
+    params['containerid'] = 1326
+    params['crystalid'] = 3918
+    params['name'] = 'Sample-010101'
+    params['code'] = 'SAM-010101'
+    id = core.upsert_sample(conn, params.values())
+    conn.disconnect()
+    assert id is not None
+
 def test_retrieve_visit_id():
     conn = get_connection()
     id = core.retrieve_visit_id(conn, 'cm14451-2')
     conn.disconnect()
     assert id == 55168
-
-def test_retrieve_proposal_title():
-    conn = get_connection()
-    title = core.retrieve_proposal_title(conn, 'cm', 14451)
-    conn.disconnect()
-    assert title.strip() == 'I03 Commissioning Directory 2016'
 
 def test_retrieve_current_sessions():
     conn = get_connection()
@@ -54,12 +59,8 @@ def test_retrieve_active_plates():
     conn.disconnect()
     assert len(rs) >= 0
 
-def test_upsert_sample():
+def test_retrieve_proposal_title():
     conn = get_connection()
-    params = core.get_sample_params()
-    params['containerid'] = 1326
-    params['crystalid'] = 3918
-    params['name'] = 'Sample-010101'
-    params['code'] = 'SAM-010101'
-    id = core.upsert_sample(conn, params.values())
+    title = core.retrieve_proposal_title(conn, 'cm', 14451)
     conn.disconnect()
+    assert title.strip() == 'I03 Commissioning Directory 2016'
