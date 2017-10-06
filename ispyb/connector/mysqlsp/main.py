0d275146ce8fc793caa23da4df00936480f95cb8
Karl Levik <karl.levik@diamond.ac.uk>
2017-10-06 11:46:14 +0100

    Use config 'db' section, rm 'section' param from signatures

--- ispyb/connector/mysqlsp/main.py
+++ ispyb/connector/mysqlsp/main.py
@@ -15,15 +15,16 @@ class ISPyBMySQLSPConnector(ispyb.interface.connection.IF):
   '''Provides a connector to an ISPyB MySQL/MariaDB database through stored procedures.
   '''
 
-  def __init__(self, section='dev', conf_file=None):
-    if not section is None and not conf_file is None:
-        self.connect(section, conf_file)
+  def __init__(self, conf_file=None):
+    if not conf_file is None:
+        self.connect(conf_file)
 
-  def connect(self, section, conf_file):
+  def connect(self, conf_file):
     self.disconnect()
     self.config = ConfigParser.ConfigParser(allow_no_value=True)
     self.config.readfp(codecs.open(conf_file, "r", "utf8"))
 
+    section = 'db'
     '''Create a connection to the database using the given parameters.'''
     self.conn = mysql.connector.connect(user=self.config.get(section, 'user'),
         password=self.config.get(section, 'pw'), \
--- tests/testtools.py
+++ tests/testtools.py
@@ -5,30 +5,30 @@ conf_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '../conf/con
 
 def get_core():
     core = get_data_area_object(DataAreaType.CORE, ConnectionType.ISPYBMYSQLSP)
-    core.get_connection().connect('dev', conf_file)
+    core.get_connection().connect(conf_file)
     return core
 
 def get_mxacquisition():
     mxacquisition = get_data_area_object(DataAreaType.MXACQUISITION, ConnectionType.ISPYBMYSQLSP)
-    mxacquisition.get_connection().connect('dev', conf_file)
+    mxacquisition.get_connection().connect(conf_file)
     return mxacquisition
 
 def get_emacquisition():
     emacquisition = get_data_area_object(DataAreaType.EMACQUISITION, ConnectionType.ISPYBMYSQLSP)
-    emacquisition.get_connection().connect('dev', conf_file)
+    emacquisition.get_connection().connect(conf_file)
     return emacquisition
 
 def get_mxprocessing():
     mxprocessing = get_data_area_object(DataAreaType.MXPROCESSING, ConnectionType.ISPYBMYSQLSP)
-    mxprocessing.get_connection().connect('dev', conf_file)
+    mxprocessing.get_connection().connect(conf_file)
     return mxprocessing
 
 def get_mxscreening():
     mxscreening = get_data_area_object(DataAreaType.MXSCREENING, ConnectionType.ISPYBMYSQLSP)
-    mxscreening.get_connection().connect('dev', conf_file)
+    mxscreening.get_connection().connect(conf_file)
     return mxscreening
 
 def get_shipping():
     shipping = get_data_area_object(DataAreaType.SHIPPING, ConnectionType.ISPYBMYSQLSP)
-    shipping.get_connection().connect('dev', conf_file)
+    shipping.get_connection().connect(conf_file)
     return shipping
