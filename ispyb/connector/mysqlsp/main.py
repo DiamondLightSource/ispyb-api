bdc123af95fa69a1a2e3d23f2778141e70d43bc5
Karl Levik <karl.levik@diamond.ac.uk>
2017-10-16 16:34:04 +0100

    Make thread safe

--- ispyb/connector/mysqlsp/main.py
+++ ispyb/connector/mysqlsp/main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import threading
 import time
 import os
 import sys
@@ -11,6 +12,7 @@ class ISPyBMySQLSPConnector(ispyb.interface.connection.IF):
   '''
 
   def __init__(self, user=None, pw=None, host='localhost', db=None, port=3306):
+    self.lock = threading.Lock()
     self.connect(user=user, pw=pw, host=host, db=db, port=port)
 
   def connect(self, user=None, pw=None, host='localhost', db=None, port=3306):
@@ -32,47 +34,44 @@ class ISPyBMySQLSPConnector(ispyb.interface.connection.IF):
     	self.conn.close()
     self.conn = None
 
-  def cursor(self, dictionary=False):
-    if hasattr(self, 'conn') and self.conn is not None:
-        return self.conn.cursor(dictionary=dictionary)
-    raise Exception('No database connection')
-
   def get_data_area_package(self):
     return 'ispyb.sp'
 
   def call_sp_write(self, procname, args):
-    cursor = self.cursor()
-    result_args = cursor.callproc(procname=procname, args=args)
-    cursor.close()
+    with self.lock:
+        cursor = self.conn.cursor()
+        result_args = cursor.callproc(procname=procname, args=args)
+        cursor.close()
     if result_args is not None and len(result_args) > 0:
         return result_args[0]
 
   def call_sp_retrieve(self, procname, args):
-    cursor = self.cursor(dictionary=True)
-    cursor.callproc(procname=procname, args=args)
-    result = []
-    for recordset in cursor.stored_results():
-        if isinstance(cursor, mysql.connector.cursor.MySQLCursorDict):
-            for row in recordset:
-                result.append(dict(list(zip(recordset.column_names,row))))
-        else:
-            result = recordset.fetchall()
-#    cursor.nextset()
-    cursor.close()
+    with self.lock:
+        cursor = self.conn.cursor(dictionary=True)
+        cursor.callproc(procname=procname, args=args)
+        result = []
+        for recordset in cursor.stored_results():
+            if isinstance(cursor, mysql.connector.cursor.MySQLCursorDict):
+                for row in recordset:
+                    result.append(dict(list(zip(recordset.column_names,row))))
+            else:
+                result = recordset.fetchall()
+        cursor.close()
     return result
 
   def call_sf(self, funcname, args):
-    cursor = self.cursor()
-    cursor.execute(('select %s' % funcname) + ' (%s)' % ','.join(['%s'] * len(args)), args)
-    result = None
-    rs = cursor.fetchone()
-    if len(rs) > 0:
-        if isinstance(cursor, mysql.connector.cursor.MySQLCursorDict):
-            result = iter(rs.items()).next()[1]
-        else:
-            try:
-                result = int(rs[0])
-            except:
-                result = rs[0]
-    cursor.close()
+    with self.lock:
+        cursor = self.conn.cursor()
+        cursor.execute(('select %s' % funcname) + ' (%s)' % ','.join(['%s'] * len(args)), args)
+        result = None
+        rs = cursor.fetchone()
+        if len(rs) > 0:
+            if isinstance(cursor, mysql.connector.cursor.MySQLCursorDict):
+                result = iter(rs.items()).next()[1]
+            else:
+                try:
+                    result = int(rs[0])
+                except:
+                    result = rs[0]
+        cursor.close()
     return result
