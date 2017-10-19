7d33422a46a36958f38d0c1e9a346e0bb485f670
Karl Levik <karl.levik@diamond.ac.uk>
2017-10-19 22:40:27 +0100

    Tidy

--- tests/test_mxacquisition.py
+++ tests/test_mxacquisition.py
@@ -33,10 +33,7 @@ def test_mxacquisition_methods():
     assert id1 == id2
 
     rs = mxacquisition.retrieve_data_collection_main(id1)
-    print(rs)
     assert rs[0]['groupId'] == dcgid
-#    assert rs[0]['axis_start'] == 0
-#    assert rs[0]['axis_end'] == 90
 
     params = mxacquisition.get_image_params()
     params['parentid'] = id1
