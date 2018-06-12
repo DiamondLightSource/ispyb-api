5352d96363cfd116a67778989122f3b2b0b1a963
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-06-12 08:54:21 +0100

    Add a helper class to encapsulate values
    A class to keep an internal value and pretend to be just like it. This
    class can be used to extend native python classes (eg. strings) with
    additional methods, but let them behave like strings in all other
    contexts.

---
+++ tests/model/test_encapsulation.py
@@ -0,0 +1,56 @@
+from __future__ import absolute_import, division, print_function
+
+import ispyb.model
+import pytest
+
+@pytest.mark.parametrize('thing', ['string', u'unicode', '', u''])
+def test_encapsulated_string_behaves_as_string(thing):
+  e = ispyb.model.EncapsulatedValue(thing)
+  assert e is not thing
+  assert e.value is thing
+
+  assert (e == thing) is True
+  assert (e != thing) is False
+  assert (e < thing) is False
+  assert (e > thing) is False
+  assert (e <= thing) is True
+  assert (e >= thing) is True
+  assert bool(e) == bool(thing)
+
+  assert str(e) == str(thing)
+  assert repr(e) != repr(thing)
+  assert list(e) == list(thing)
+
+  for comparison in ('strinf', 'string', 'strinh', u'unicodd', u'unicode', u'unicodf'):
+    assert (e == comparison) is (thing == comparison)
+    assert (e <= comparison) is (thing <= comparison)
+    assert (e >= comparison) is (thing >= comparison)
+    assert (e < comparison) is (thing < comparison)
+    assert (e > comparison) is (thing > comparison)
+
+@pytest.mark.parametrize('thing', [0, 2])
+def test_encapsulated_number_behaves_as_number(thing):
+  e = ispyb.model.EncapsulatedValue(thing)
+  assert e is not thing
+  assert e.value is thing
+
+  assert (e == thing) is True
+  assert (e != thing) is False
+  assert (e < thing) is False
+  assert (e > thing) is False
+  assert (e <= thing) is True
+  assert (e >= thing) is True
+  assert bool(e) == bool(thing)
+
+  assert str(e) == str(thing)
+  assert repr(e) != repr(thing)
+
+  with pytest.raises(AttributeError):
+    list(e)
+
+  for comparison in (-1, 0, 1, 2, 3):
+    assert (e == comparison) is (thing == comparison)
+    assert (e <= comparison) is (thing <= comparison)
+    assert (e >= comparison) is (thing >= comparison)
+    assert (e < comparison) is (thing < comparison)
+    assert (e > comparison) is (thing > comparison)
