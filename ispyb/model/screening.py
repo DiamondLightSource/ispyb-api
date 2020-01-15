c37edc3a85ec48af0918aee3273c43165ae831bf
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-01-15 11:58:09 +0000

    Add docstrings; missing @property

--- ispyb/model/screening.py
+++ ispyb/model/screening.py
@@ -11,14 +11,15 @@ class Screening(ispyb.model.DBCache):
     """
 
     def __init__(self, screening_id, db_conn, preload=None):
-        """Create a IntegrationResult object for a defined AutoProcIntegrationID.
+        """Create a Screening object for a defined ScreeningId.
+
         Requires a database connection object exposing further data access
         methods.
 
         :param sceening_id: ScreeningId
         :param db_conn: ISPyB database connection object
-        :return: An IntegrationResult object representing the database entry for
-                 the specified AutoProcIntegrationID
+        :return: A Screening object representing the database entry for
+                 the specified ScreeningId
         """
         self._db = db_conn
         self._screening_id = int(screening_id)
@@ -29,7 +30,10 @@ class Screening(ispyb.model.DBCache):
         """Load/update information from the database."""
         raise NotImplementedError()
 
+    @property
     def screening_outputs(self):
+        """Returns the list of ScreeningOutput objects associated with this
+           database entry."""
         raise NotImplementedError()
 
 
@@ -44,7 +48,22 @@ ispyb.model.add_properties(
 
 
 class ScreeningOutput(ispyb.model.DBCache):
+    """An object representing a ScreeningOutput database entry.
+    The object lazily accesses the underlying database when necessary and
+    exposes record data as python attributes.
+    """
+
     def __init__(self, output_id, db_conn, preload=None):
+        """Create a ScreeningOutput object for a defined ScreeningOutputId.
+
+        Requires a database connection object exposing further data access
+        methods.
+
+        :param output_id: ScreeningOutputId
+        :param db_conn: ISPyB database connection object
+        :return: A ScreeningOutput object representing the database entry for
+                 the specified ScreeningOutputId
+        """
         self._db = db_conn
         self._output_id = output_id
         if preload:
@@ -54,10 +73,16 @@ class ScreeningOutput(ispyb.model.DBCache):
         """Load/update information from the database."""
         raise NotImplementedError()
 
+    @property
     def lattices(self):
+        """Returns the list of ScreeningOutputLattice objects associated with
+           this database entry."""
         raise NotImplementedError()
 
+    @property
     def strategies(self):
+        """Returns the list of ScreeningStrategy objects associated with this
+           database entry."""
         raise NotImplementedError()
 
 
@@ -73,7 +98,22 @@ ispyb.model.add_properties(
 
 
 class ScreeningOutputLattice(ispyb.model.DBCache):
+    """An object representing a ScreeningOutputLattice database entry.
+    The object lazily accesses the underlying database when necessary and
+    exposes record data as python attributes.
+    """
+
     def __init__(self, lattice_id, db_conn, preload=None):
+        """Create a ScreeningOutputLattice object for a defined ScreeningOutputLatticeId.
+
+        Requires a database connection object exposing further data access
+        methods.
+
+        :param lattice_id: ScreeningOutputLatticeId
+        :param db_conn: ISPyB database connection object
+        :return: A ScreeningOutputLattice object representing the database entry
+                 for the specified ScreeningOutputLatticeId
+        """
         self._db = db_conn
         self._lattice_id = lattice_id
         if preload:
@@ -85,7 +125,7 @@ class ScreeningOutputLattice(ispyb.model.DBCache):
 
     @property
     def unit_cell(self):
-        """Returns the unit cell model"""
+        """Returns the unit cell model."""
         return UnitCell(
             self._data["unitCell_a"],
             self._data["unitCell_b"],
@@ -100,7 +140,22 @@ ispyb.model.add_properties(ScreeningOutputLattice, (("spacegroup", "spaceGroup")
 
 
 class ScreeningStrategy(ispyb.model.DBCache):
+    """An object representing a ScreeningStrategy database entry.
+    The object lazily accesses the underlying database when necessary and
+    exposes record data as python attributes.
+    """
+
     def __init__(self, strategy_id, db_conn, preload=None):
+        """Create a ScreeningStrategy object for a defined ScreeningStrategyId.
+
+        Requires a database connection object exposing further data access
+        methods.
+
+        :param strategy_id: ScreeningStrategyId
+        :param db_conn: ISPyB database connection object
+        :return: A ScreeningStrategy object representing the database entry for
+                 the specified ScreeningStrategyId
+        """
         self._db = db_conn
         self._strategy_id = strategy_id
         if preload:
@@ -110,7 +165,10 @@ class ScreeningStrategy(ispyb.model.DBCache):
         """Load/update information from the database."""
         raise NotImplementedError()
 
+    @property
     def wedges(self):
+        """Returns the list of ScreeningStrategyWedge objects associated with
+           this database entry."""
         raise NotImplementedError()
 
 
@@ -126,7 +184,22 @@ ispyb.model.add_properties(
 
 
 class ScreeningStrategyWedge(ispyb.model.DBCache):
+    """An object representing a ScreeningStrategyWedge database entry.
+    The object lazily accesses the underlying database when necessary and
+    exposes record data as python attributes.
+    """
+
     def __init__(self, strategy_wedge_id, db_conn, preload=None):
+        """Create a ScreeningStrategyWedge object for a defined ScreeningStrategyWedgeId.
+
+        Requires a database connection object exposing further data access
+        methods.
+
+        :param strategy_wedge_id: ScreeningStrategyWedgeId
+        :param db_conn: ISPyB database connection object
+        :return: A ScreeningStrategyWedge object representing the database entry
+                 for the specified ScreeningStrategyWedgeId
+        """
         self._db = db_conn
         self._strategy_wedge_id = strategy_wedge_id
         if preload:
@@ -136,7 +209,10 @@ class ScreeningStrategyWedge(ispyb.model.DBCache):
         """Load/update information from the database."""
         raise NotImplementedError()
 
+    @property
     def sub_wedges(self):
+        """Returns the list of ScreeningStrategySubWedge objects associated with
+           this database entry."""
         raise NotImplementedError()
 
 
@@ -156,7 +232,22 @@ ispyb.model.add_properties(
 
 
 class ScreeningStrategySubWedge(ispyb.model.DBCache):
+    """An object representing a ScreeningStrategySubWedge database entry.
+    The object lazily accesses the underlying database when necessary and
+    exposes record data as python attributes.
+    """
+
     def __init__(self, strategy_sub_wedge_id, db_conn, preload=None):
+        """Create a ScreeningStrategySubWedge object for a defined ScreeningStrategySubWedgeId.
+
+        Requires a database connection object exposing further data access
+        methods.
+
+        :param strategy_sub_wedge_id: ScreeningStrategySubWedgeId
+        :param db_conn: ISPyB database connection object
+        :return: A ScreeningStrategySubWedge object representing the database entry
+                 for the specified ScreeningStrategySubWedgeId
+        """
         self._db = db_conn
         self._strategy_sub_wedge_id = strategy_sub_wedge_id
         if preload:
