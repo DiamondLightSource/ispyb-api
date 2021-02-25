f94a1f88e330805ed30eb935f2f7a5d0b515245a
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-02-25 14:28:26 +0000

    Add ProcessingJob* relationships
    * ProcessingJob.ProcessingJobParameters
    * ProcessingJob.ProcessingJobImageSweeps

--- ispyb/sqlalchemy/__init__.py
+++ ispyb/sqlalchemy/__init__.py
@@ -1,8 +1,10 @@
 from sqlalchemy.orm import relationship
 
 from ._auto_db_schema import *  # noqa F403
-from ._auto_db_schema import AutoProcProgram, AutoProcScaling
+from ._auto_db_schema import AutoProcProgram, AutoProcScaling, ProcessingJob
 
 
 AutoProcProgram.AutoProcProgramAttachments = relationship("AutoProcProgramAttachment")
 AutoProcScaling.AutoProcScalingStatistics = relationship("AutoProcScalingStatistics")
+ProcessingJob.ProcessingJobParameters = relationship("ProcessingJobParameter")
+ProcessingJob.ProcessingJobImageSweeps = relationship("ProcessingJobImageSweep")
--- tests/sqlalchemy/test_models.py
+++ tests/sqlalchemy/test_models.py
@@ -1,9 +1,12 @@
 import datetime
+import pytest
+
 from ispyb.sqlalchemy import (
     AutoProcProgram,
     AutoProcScaling,
     DataCollection,
     DataCollectionGroup,
+    ProcessingJob,
 )
 
 
@@ -60,3 +63,73 @@ def test_auto_proc_program(testsqlalchemy):
     assert len(attachments) == 1
     assert attachments[0].fileName == "fast_dp.log"
     assert attachments[0].fileType == "Log"
+
+
+@pytest.fixture
+def processing_jobs(testdb):
+    # Add some ProcessingJob* entries
+    mxprocessing = testdb.mx_processing
+
+    params = mxprocessing.get_job_params()
+    params.update(
+        dict(
+            datacollectionid=993677,
+            display_name="test_job",
+            comments="1 2 3 testing",
+            automatic=True,
+            recipe="xia2-dials",
+        )
+    )
+    job_id = mxprocessing.upsert_job(list(params.values()))
+    assert job_id is not None
+    assert job_id > 0
+
+    params = mxprocessing.get_job_parameter_params()
+    params.update(dict(job_id=job_id, parameter_key="pi", parameter_value="3.14"))
+    jp_id = mxprocessing.upsert_job_parameter(list(params.values()))
+    assert jp_id is not None
+    assert jp_id > 0
+
+    params = mxprocessing.get_job_image_sweep_params()
+    params.update(
+        dict(
+            job_id=job_id,
+            datacollectionid=993677,
+            start_image=1,
+            end_image=180,
+        )
+    )
+    jis_id = mxprocessing.upsert_job_image_sweep(list(params.values()))
+    assert jis_id is not None
+    assert jis_id > 0
+    return job_id
+
+
+def test_processing_job(testsqlalchemy, processing_jobs):
+    pj_id = processing_jobs
+
+    query = testsqlalchemy.query(ProcessingJob).filter(
+        ProcessingJob.processingJobId == pj_id
+    )
+    assert query.count() == 1
+    pj = query.first()
+    assert pj.processingJobId == pj_id
+    pj.recipe == "xia2-dials"
+    assert pj.automatic
+
+    dc = pj.DataCollection
+    assert dc is not None
+    assert pj.dataCollectionId == 993677
+
+    assert len(pj.ProcessingJobParameters) == 1
+    pjp = pj.ProcessingJobParameters[0]
+    assert pjp.processingJobId == pj.processingJobId
+    assert pjp.parameterKey == "pi"
+    assert pjp.parameterValue == "3.14"
+
+    assert len(pj.ProcessingJobImageSweeps) == 1
+    pjis = pj.ProcessingJobImageSweeps[0]
+    assert pjis.dataCollectionId == pj.dataCollectionId
+    assert pjis.DataCollection is pj.DataCollection
+    assert pjis.startImage == 1
+    assert pjis.endImage == 180
