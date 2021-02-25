8b40dd4e952a2eefdfdb2b3a0793d91608f5d378
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-02-25 13:35:54 +0000

    Add AutoProc* relationships

--- ispyb/sqlalchemy/__init__.py
+++ ispyb/sqlalchemy/__init__.py
@@ -1,1 +1,8 @@
+from sqlalchemy.orm import relationship
+
 from ._auto_db_schema import *  # noqa F403
+from ._auto_db_schema import AutoProcProgram, AutoProcScaling
+
+
+AutoProcProgram.AutoProcProgramAttachments = relationship("AutoProcProgramAttachment")
+AutoProcScaling.AutoProcScalingStatistics = relationship("AutoProcScalingStatistics")
--- tests/sqlalchemy/test_models.py
+++ tests/sqlalchemy/test_models.py
@@ -1,4 +1,10 @@
-from ispyb.sqlalchemy import DataCollection, DataCollectionGroup
+import datetime
+from ispyb.sqlalchemy import (
+    AutoProcProgram,
+    AutoProcScaling,
+    DataCollection,
+    DataCollectionGroup,
+)
 
 
 def test_data_collection(testsqlalchemy):
@@ -19,3 +25,38 @@ def test_data_collection_group(testsqlalchemy):
     assert query.count() == 5
     dcg = query.first()
     assert dcg.dataCollectionGroupId == 988855
+
+
+def test_auto_proc_scaling(testsqlalchemy):
+    query = testsqlalchemy.query(AutoProcScaling)
+    assert query.count() == 11
+    aps = query.first()
+    assert aps.autoProcScalingId == 596133
+    assert aps.recordTimeStamp == datetime.datetime(2016, 1, 14, 12, 46, 22)
+
+    # Get child AutoProcScalingStatistics entries
+    stats = aps.AutoProcScalingStatistics
+    assert len(stats)
+    assert stats[0].ccHalf == 99.9
+    assert stats[0].autoProcScalingStatisticsId == 1770619
+    assert stats[0].autoProcScalingId == aps.autoProcScalingId
+
+    # Get parent AutoProc entry
+    ap = aps.AutoProc
+    assert ap.autoProcId == aps.autoProcId
+    assert ap.refinedCell_a == 92.5546
+
+
+def test_auto_proc_program(testsqlalchemy):
+    query = testsqlalchemy.query(AutoProcProgram)
+    assert query.count() == 11
+    app = query.first()
+    assert app.autoProcProgramId == 56425592
+    assert app.processingPrograms == "fast_dp"
+    assert app.ProcessingJob is None
+
+    # Get child AutoProcProgramAttachments entries
+    attachments = app.AutoProcProgramAttachments
+    assert len(attachments) == 1
+    assert attachments[0].fileName == "fast_dp.log"
+    assert attachments[0].fileType == "Log"
