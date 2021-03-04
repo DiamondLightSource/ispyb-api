bbb3e976688663e490ca27aab31d5c2ccde889f2
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-03-04 18:09:09 +0000

    Remove deprecated features for 6.0 release

--- ispyb/factory.py
+++ ispyb/factory.py
@@ -1,5 +1,3 @@
-import importlib
-import ispyb
 from enum import Enum
 
 
@@ -16,32 +14,3 @@ class DataAreaType(Enum):
         self.description = description
         self.module = module
         self.classname = classname
-
-
-def create_connection(conf_file):
-    import warnings
-
-    warnings.warn("deprecated, use ispyb.open()", DeprecationWarning)
-    return ispyb.open(conf_file)
-
-
-def create_data_area(data_area_type, conn):
-    """Factory function. Given a DataArea type and a Connection object imports the relevant data area module and
-    returns the correct type of Data Area object with its connection as the Connection object."""
-    import warnings
-
-    warnings.warn(
-        "deprecated, use the data area properties on the connection object",
-        DeprecationWarning,
-    )
-    if not hasattr(data_area_type, "module") or not hasattr(
-        data_area_type, "classname"
-    ):
-        raise AttributeError("DataArea type %s does not exist" % data_area_type)
-    da_mod = importlib.import_module(
-        f"{conn.get_data_area_package()}.{data_area_type.module}"
-    )
-    DAClass = getattr(da_mod, data_area_type.classname)
-    da = DAClass()
-    da.set_connection(conn)
-    return da
