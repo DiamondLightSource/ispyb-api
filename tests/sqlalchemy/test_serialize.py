0f18d38c40c0827575b2ea314101294e407cbef0
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-03-04 14:53:58 +0000

    Slightly simplify test

--- tests/sqlalchemy/test_serialize.py
+++ tests/sqlalchemy/test_serialize.py
@@ -2,13 +2,8 @@ from ispyb.sqlalchemy import DataCollection
 
 
 def test_data_collection(alchemy):
-    query = (
-        alchemy.query(DataCollection)
-        .order_by(DataCollection.dataCollectionId)
-        .filter_by(SESSIONID=55168)
-    )
-    assert query.count() == 3
-    dc = query.first()
+    query = alchemy.query(DataCollection).filter_by(dataCollectionId=1052494)
+    dc = query.one()
     dc_schema = DataCollection.__marshmallow__()
     dump_data = dc_schema.dump(dc)
     # Test that SQLAlchemyAutoSchema has include_fk=True
