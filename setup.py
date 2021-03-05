9cd1e54a0050e5c1d9ae758f4bc623f4a419b345
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-03-05 13:58:48 +0000

    more setup.py fine-tuning

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(
-        author="Karl Erik Levik, Markus Gerstel",
-        author_email="scientificsoftware@diamond.ac.uk",
-        scripts=["bin/dimple2ispyb.py", "bin/mxdatareduction2ispyb.py"],
-        tests_require=["pytest"],
-    )
+    setuptools.setup()
