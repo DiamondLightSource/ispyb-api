8ce3a9df407b495fd6d06556249d1b5ecd49718c
Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>
2021-03-05 15:46:59 +0000

    Change package to /src layout (#133)
    This moves all source files into /src/ispyb and thus resolves #131.
    This source layout provides a clean package boundary and prevents importing
    non-package files such as setup.py and the /bin directory.
    
    setup.py / setup.cfg were updated accordingly to support the packaging change,
    and most package metadata has now moved into the .cfg file. 
    
    The package used to be described as 'beta', but we have been treating it as production
    for a long time now, so we might as well change that descriptor to 'stable'.

--- setup.py
+++ setup.py
@@ -1,42 +1,4 @@
-from __future__ import absolute_import, division, print_function
+import setuptools
 
-from setuptools import find_packages, setup
-
-INSTALL_REQUIRES = ["mysql-connector-python", "sqlalchemy", "tabulate"]
-
-setup(
-    name="ispyb",
-    version="5.9.1",
-    description="Python package to access ISPyB database",
-    long_description="This package provides a python interface to ISPyB. It can access the ISPyB database directly or (in future versions) run on top of the official ISPyB webservices API.",
-    url="https://github.com/DiamondLightSource/ispyb-api",
-    author="Karl Erik Levik, Markus Gerstel",
-    author_email="scientificsoftware@diamond.ac.uk",
-    download_url="https://github.com/DiamondLightSource/ispyb-api/releases",
-    keywords=["ISPyB", "database"],
-    packages=find_packages(),
-    scripts=["bin/dimple2ispyb.py", "bin/mxdatareduction2ispyb.py"],
-    license="Apache License, Version 2.0",
-    install_requires=INSTALL_REQUIRES,
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    python_requires=">=3.6",
-    entry_points={
-        "libtbx.precommit": ["ispyb = ispyb"],
-        "console_scripts": [
-            "ispyb.last_data_collections_on=ispyb.cli.last_data_collections_on:main",
-        ],
-    },
-    project_urls={"Documentation": "https://ispyb.readthedocs.io"},
-)
+if __name__ == "__main__":
+    setuptools.setup()
