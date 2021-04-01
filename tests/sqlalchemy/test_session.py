1ffd9950a3b22d2303d1062a68555a5a13ddb7d0
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-04-01 09:12:32 +0100

    Explicit tests for ispyb.sqlalchemy.url

--- tests/sqlalchemy/test_session.py
+++ tests/sqlalchemy/test_session.py
@@ -18,3 +18,20 @@ def test_session_from_envvar(testconfig, monkeypatch):
     monkeypatch.setenv("ISPYB_CREDENTIALS", testconfig)
     session = ispyb.sqlalchemy.session()
     assert isinstance(session, sqlalchemy.orm.session.Session)
+
+
+def test_url_from_dict(testconfig):
+    config = configparser.RawConfigParser(allow_no_value=True)
+    config.read(testconfig)
+    url = ispyb.sqlalchemy.url(credentials=dict(config.items("ispyb_sqlalchemy")))
+    assert url.startswith("mysql+mysqlconnector")
+    # check we can create a valid engine with this url
+    sqlalchemy.create_engine(url, connect_args={"use_pure": True})
+
+
+def test_url_from_envar(testconfig, monkeypatch):
+    monkeypatch.setenv("ISPYB_CREDENTIALS", testconfig)
+    url = ispyb.sqlalchemy.url()
+    assert url.startswith("mysql+mysqlconnector")
+    # check we can create a valid engine with this url
+    sqlalchemy.create_engine(url, connect_args={"use_pure": True})
