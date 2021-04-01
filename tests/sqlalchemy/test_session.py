8131aa9ca59fbf8042b9aabdbe162e7cf094e396
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-04-01 09:16:57 +0100

    Account for deprecation warning in tests

--- tests/sqlalchemy/test_session.py
+++ tests/sqlalchemy/test_session.py
@@ -1,4 +1,5 @@
 import configparser
+import pytest
 
 import sqlalchemy.orm
 
@@ -8,15 +9,17 @@ import ispyb.sqlalchemy
 def test_session_from_dict(testconfig):
     config = configparser.RawConfigParser(allow_no_value=True)
     config.read(testconfig)
-    session = ispyb.sqlalchemy.session(
-        credentials=dict(config.items("ispyb_sqlalchemy"))
-    )
+    with pytest.warns(PendingDeprecationWarning):
+        session = ispyb.sqlalchemy.session(
+            credentials=dict(config.items("ispyb_sqlalchemy"))
+        )
     assert isinstance(session, sqlalchemy.orm.session.Session)
 
 
 def test_session_from_envvar(testconfig, monkeypatch):
     monkeypatch.setenv("ISPYB_CREDENTIALS", testconfig)
-    session = ispyb.sqlalchemy.session()
+    with pytest.warns(PendingDeprecationWarning):
+        session = ispyb.sqlalchemy.session()
     assert isinstance(session, sqlalchemy.orm.session.Session)
 
 
